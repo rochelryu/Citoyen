304d6059041cc596e0174e6f7c440b865e78d636
oraclerevolution <mac.n.drix@gmail.com>
2019-02-05 16:37:17 +0000

    ajout de la modal

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Text, StyleSheet, Image, TouchableOpacity, Modal} from 'react-native'
+import {View, Text, StyleSheet, Image, TouchableOpacity, Modal, Alert, StyleSheet} from 'react-native'
 import { Header, Left, Button, Right, Icon } from 'native-base';
 
 const styles = StyleSheet.create({
@@ -39,10 +39,47 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width: 150,
         height: 170
-    }
+    },
+    modal:{
+        position: 'relative',
+        bottom: "-15%",
+        textAlign: 'center',
+        margin:30,
+        borderRadius:5,
+        display: 'flex',
+        justifyContent:"center",
+        alignItems: "center",
+        padding:16
+      },
+      bouton: {
+        color: 'white',
+        textAlign:'center',
+        borderColor:'#000000',
+        margin:3,
+        padding:10,
+        backgroundColor:'#F77F00',
+        width:250,
+        shadowColor: '#000000',
+        shadowOpacity: 15
+      },
+      closeModal:{
+        
+        marginTop:15,
+        color: 'red',
+        borderWidth: 1,
+        borderColor: 'red',
+        padding:10
+      },
 })
 
 class HomeScreen extends React.Component{
+    state = {
+        modalVisible: false,
+      };
+    
+      setModalVisible(visible){
+        this.setState({modalVisible: visible});
+      }
 
     constructor(props){
         super(props)
@@ -58,6 +95,29 @@ class HomeScreen extends React.Component{
     render(){
         return (
         <View style={styles.container}>
+            {/* modal d'affichage */}
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={this.state.modalVisible}
+                onRequestClose={() => {
+                Alert.alert('Modal has been closed');
+            }}>
+                <View style={{marginTop: 22}}>
+                    <View style = {styles.modal}>
+                    <Text style= {styles.bouton}> RECLAMATION </Text>
+                    <Text style= {styles.bouton}> OBSERVATION </Text>
+                    <Text style= {styles.bouton}> SUGGESTION </Text>
+                    <TouchableHighlight
+                    onPress={() => {
+                        this.setModalVisible(!this.state.modalVisible);
+                    }}>
+                    <Text style={styles.closeModal}>FERMER</Text>
+                    </TouchableHighlight>
+                    </View>
+                </View>
+            </Modal>
+
             <Header style={{backgroundColor:'#00a25d'}}>
                 <Left>
                     <Button transparent>
@@ -76,7 +136,7 @@ class HomeScreen extends React.Component{
                 <Image style={styles.Icon} source={require('../assets/iconT.png')}/>
             </View>
             <View style={styles.trans}>
-                <TouchableOpacity onPress={()=> console.log("test")}>
+                <TouchableOpacity onPress={() => this.setModalVisible(true)}>
                     <Image style={styles.img} source={require('../assets/smc.png')}/>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={()=> console.log('je clique vraiment 2')}>
@@ -88,7 +148,7 @@ class HomeScreen extends React.Component{
             <TouchableOpacity onPress={()=> console.log('je clique vraiment 2')}>
                 <Image style={styles.img} source={require('../assets/x.png')}/>
             </TouchableOpacity>
-            <TouchableOpacity onPress={()=> this.props.navigation('Suivie')}>
+            <TouchableOpacity onPress={()=> console.log(naviget)}>
                 <Image style={styles.img1} source={require('../assets/suivre.png')}/>
             </TouchableOpacity>
             </View>
